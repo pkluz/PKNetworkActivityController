466de3de66aef162e65809ad8c02cd0d3d0749e5
Philip Kluz <Philip.Kluz@zuui.org>
2014-06-19 20:41:50 +0200

    Initial Commit

---
+++ PKNetworkActivity/PKNetworkActivity.h
@@ -0,0 +1,19 @@
+//
+//  PKNetworkActivity.h
+//  PKNetworkActivity
+//
+//  Created by Philip Kluz on 6/19/14.
+//  Copyright (c) 2014 NSExceptional. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PKNetworkActivity.
+FOUNDATION_EXPORT double PKNetworkActivityVersionNumber;
+
+//! Project version string for PKNetworkActivity.
+FOUNDATION_EXPORT const unsigned char PKNetworkActivityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PKNetworkActivity/PublicHeader.h>
+
+
