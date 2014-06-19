e8ff3e5de448514eb2e06a680d229056ac1f7843
Philip Kluz <Philip.Kluz@zuui.org>
2014-06-19 22:01:56 +0200

    Added gitignore

---
+++ PKNetworkActivity/PKNetworkActivityController.swift
@@ -0,0 +1,9 @@
+//
+//  PKNetworkActivity.swift
+//  PKNetworkActivity
+//
+//  Created by Philip Kluz on 6/19/14.
+//  Copyright (c) 2014 NSExceptional. All rights reserved.
+//
+
+import Foundation
